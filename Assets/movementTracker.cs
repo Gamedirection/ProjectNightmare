d8846d6d5868c6f488f7bb13330e6f3b5ae67c57
Liam <leeman1230@yahoo.ca>
2017-11-12 22:39:41 -0500

    Wall movement working with constraints, feels a little rough so I need to smooth it out a bit

---
+++ Assets/movementTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementTracker : MonoBehaviour {
+
+    [HideInInspector]
+    public int verticalCount;           // Tracks how far vertically the wall has moved this round
+    [HideInInspector]
+    public int horizontalCount;         // Tracks how far horizontally the wall has moved this round
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called at the start of the scene
+    void Start () {
+        verticalCount = 0;      // Start count at 0
+        horizontalCount = 0;    // Start count at 0
+	}
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called every frame
+    void Update () {
+        //Lock position of wall to an integer
+        transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), Mathf.RoundToInt(transform.position.z));
+    }
+}
--- Assets/wallManager.cs
+++ Assets/wallManager.cs
@@ -4,21 +4,73 @@ using UnityEngine;
 
 public class wallManager : MonoBehaviour {
 
-    public bool roundActive;
+    [HideInInspector]
+    public bool roundActive;                // Tracks if the round is currently active or is inbetween rounds
 
-    private GameObject selectedWall;
+    private GameObject selectedWall;        // Object reference for the selected wall the player can move
+    private int verticalMoveCount;          // Total vertical movement the wall has completed
+    private int horizontalMoveCount;        // Total vertical movement the wall has completed
 
-	// Use this for initialization
-	void Start () {
-		
+    [Header("Wall Options")]
+    public int maxMoves;                    // Maximum number of times the wall can move in one round
+    private bool canMoveLeft;               //
+    private bool canMoveRight;
+    private bool canMoveUp;
+    private bool canMoveDown;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called at the start of the scene
+    void Start () {
+        //Error logging
+		if (maxMoves < 1) { Debug.LogError("DEVELOPER ERROR - Bad Variable - Max moves not set to a valid number on " + gameObject.name); }
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called every frame
+    void Update () {
+        //Check for the wall being within a valid movement distance, and set movement allowance accordingly
+        if (Mathf.Abs(verticalMoveCount + horizontalMoveCount) < maxMoves)
+        {
+            canMoveDown = true;
+            canMoveLeft = true;
+            canMoveRight = true;
+            canMoveUp = true;
+        } else {
+            canMoveDown = false;
+            canMoveLeft = false;
+            canMoveRight = false;
+            canMoveUp = false;
+            setValidDirections();
+        }
+
         checkForWallSelect();
         checkForMovement();
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Determines which directions are valid for the player to move given that we have are at the maximum movement distance for the round
+    private void setValidDirections ()
+    {
+        if (verticalMoveCount > 0)
+        {
+            canMoveDown = true;
+        }
+        if (verticalMoveCount < 0)
+        {
+            canMoveUp = true;
+        }
+        if (horizontalMoveCount > 0)
+        {
+            canMoveLeft = true;
+        }
+        if (horizontalMoveCount < 0)
+        {
+            canMoveRight = true;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Checks for if the player has clicked on a wall in the game
     private void checkForWallSelect ()
     {
         if (Input.GetMouseButtonDown(0) && !roundActive)
@@ -27,20 +79,47 @@ public class wallManager : MonoBehaviour {
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 999999999))
             {
+                Debug.Log(hit.collider.gameObject.name);
                 if (hit.collider.gameObject.tag == "wall")
                 {
+                    Debug.Log("Hit the wall");
                     selectedWall = hit.collider.gameObject;
+                    verticalMoveCount = selectedWall.GetComponent<movementTracker>().verticalCount;
+                    horizontalMoveCount = selectedWall.GetComponent<movementTracker>().horizontalCount;
                 }
             }
         }
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Moves a wall if it has been selected by the player
     private void checkForMovement ()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) { selectedWall = null; }
+        //Deselect the wall
+        if (Input.GetKeyDown(KeyCode.Escape)) {                                                                                                     // Check for the player pressing "ESCAPE"
+            selectedWall.GetComponent<movementTracker>().verticalCount = verticalMoveCount;                                                             // Update the number of times the wall has moved vertically
+            selectedWall.GetComponent<movementTracker>().horizontalCount = horizontalMoveCount;                                                         // Update the number of times the wall has moved horizontally
+            selectedWall = null;                                                                                                                        // Remove the object reference for the wall
+        }
 
-        if (Input.GetKeyDown(KeyCode.A) && selectedWall != null) {
-            selectedWall.transform.Translate(Vector3.left);
+        if (Input.GetKeyDown(KeyCode.A) && selectedWall != null && canMoveLeft) {                                                                   // If the player presses A AND the wall is not null AND they can still move the wall
+            selectedWall.transform.Translate(Vector3.left);                                                                                             // Move the wall to the left by 1
+            horizontalMoveCount--;                                                                                                                      // Decriment the wall movement total (move left)
+        }
+        if (Input.GetKeyDown(KeyCode.D) && selectedWall != null && canMoveRight)                                                                    // If the player presses D AND the wall is not null AND they can still move the wall
+        {
+            selectedWall.transform.Translate(Vector3.left * -1);                                                                                        // Move the wall to the right by 1
+            horizontalMoveCount++;                                                                                                                      // Increment the wall movement total (move right)
+        }
+        if (Input.GetKeyDown(KeyCode.W) && selectedWall != null && canMoveUp)                                                                       // If the player presses W AND the wall is not null AND they can still move the wall
+        {
+            selectedWall.transform.Translate(Vector3.forward);                                                                                               // Move the wall upwards by 1
+            verticalMoveCount++;                                                                                                                        // Increment the wall movement total (move up)
+        }
+        if (Input.GetKeyDown(KeyCode.S) && selectedWall != null && canMoveDown)                                                                     // If the player pressed S AND the wall is not null AND they can still move the wall
+        {
+            selectedWall.transform.Translate(Vector3.forward * -1);                                                                                          // Move the downwards by 1
+            verticalMoveCount--;                                                                                                                        // Decriment the wall movement total (move down)
         }
     }
 }
